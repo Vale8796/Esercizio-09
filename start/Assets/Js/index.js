cac41e2c20269560c1aa059ca67494858957bd1b
Valeria Mancuso <110185990+Vale8796@users.noreply.github.com>
2022-10-10 15:46:46 +0200

    Add files via upload

---
+++ start/Assets/Js/index.js
@@ -0,0 +1,7 @@
+alert('Ciao a tutti!');
+window.alert('Sto studiando JS');
+
+document.write('Prima prova con Javascript');
+
+document.getElementById('primo').innerHTML = 'Benvenuti in Javascript!';
+
---
+++ start/Assets/Js/naming.js
@@ -0,0 +1,21 @@
+var pet_preferito = 'gatto';
+console.log(pet_preferito);
+
+var pet = 'gatto';
+console.log(pet);
+var Pet = 'cane';
+console.log(Pet);
+var petPreferito = 'criceto';
+console.log(petPreferito);
+var PetPreferito = 'coniglio';
+console.log(PetPreferito);
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+console.log(_pet2);
+
+var numero = 12;
+console.log(12);
